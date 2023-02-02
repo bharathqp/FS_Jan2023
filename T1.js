082ed558eab90063a22aea97c39d86e212da37a6
bharathcy <bharathcy@gmail.com>
2023-02-02 10:14:27 +0530

    'updated'

---
+++ T1.js
@@ -0,0 +1,18 @@
+let height = 20
+let base   = 12.5
+const multplier = 0.5
+
+base = 25
+let output = multplier * base * height
+// console.log("The height of the triangle is = ", output)
+
+let some_float  = -12.345
+
+console.log(some_float)
+
+console.log(Math.round(some_float))
+console.log(Math.ceil(some_float))
+console.log(Math.floor(some_float))
+
+
+console.log(Math.abs(some_float))
